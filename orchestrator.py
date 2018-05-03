4afe57d2ff323f4862c0b9fd7ae324dd887b89a3
Adrien Coulier <adrien.coulier@it.uu.se>
2018-05-03 16:01:15 +0200

    Delete local storage

--- orchestrator.py
+++ orchestrator.py
@@ -158,17 +158,4 @@ for step in range(1, config["n_steps"] + 1):
                 config["data_folder"] + "/cell-{}-{}.out".format(step, cell_id),
                 input_files)
 
-from dask.diagnostics import Profiler
-
-with Profiler() as prof:
-    get(dag, [config["data_folder"] + "/cell-{}-{}.out".format(config["n_steps"], cell_id) for cell_id in network])
-
-with open(sys.argv[3], 'w') as f:
-    json.dump(
-            [{
-                key: value
-                for key, value in task._asdict().items()
-                if key in ['key', 'start_time', 'end_time']
-                }
-                for task in prof.results],
-            f)
+get(dag, [config["data_folder"] + "/cell-{}-{}.out".format(config["n_steps"], cell_id) for cell_id in network])
