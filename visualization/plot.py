3b0f82caf5a7176e444ebb89a1d122ef1d4a7685
Adrien Coulier <adrien.coulier@it.uu.se>
2018-06-29 10:51:46 +0200

    Add plot

--- visualization/plot.py
+++ visualization/plot.py
@@ -8,72 +8,65 @@ import matplotlib.animation as animation
 with open(sys.argv[1], 'r') as f:
     config = json.load(f)
 
-with open(sys.argv[2], 'r') as f:
-    network = json.load(f)
+output_folder = sys.argv[2]
 
-tstep = 1
-
-raw_data = {cell_id:{
-    "particles":[(float(re.split(" [TN]=", particles.split('\n')[0])[1]),
-        [(int(sid), (float(x), float(y), float(z)))
-        for _, sid, x, y, z in [particle.split()
-        for particle in particles.split('\n')[2:] if particle]])
-        for particles in open(config["data_folder"]+"/cell-{}-{}.out".format(tstep, cell_id),
-            'r').read().split("ParticlePositions")[1:]],
-    "cell": network[cell_id]["position"]}
-        for cell_id in network}
-
-n_local_steps = len(raw_data["1"]["particles"])
+xmin = float(sys.argv[3])
+xmax = float(sys.argv[4])
+ymin = float(sys.argv[5])
+ymax = float(sys.argv[6])
 
 color_map = {
         1: 'k',
-        2: 'b',
+        2: 'k',
         3: 'r',
-        4: 'y',
+        4: 'b',
         }
 bias = 1
 
-fig, ax = plt.subplots()
-plot = ax.scatter([], [])
+for mech_step in range(config["n_mech_steps"]):
+    with open("{}/{}.out".format(
+        config["data_folder"],
+        config["network_file"].format(step=mech_step))) as f:
+            network = json.load(f)["final"]
 
-def init():
-    ax.set_xlim(0, np.sqrt(len(network))*network["1"]["wsize"])
-    ax.set_ylim(0, np.sqrt(len(network))*network["1"]["wsize"])
-    return plot
+    for kin_step in range(config["n_kin_substeps"]):
+        plt.figure(figsize=(7.5,7.5))
+        raw_data = {cell_id:{
+            "particles":[(float(re.split(" [TN]=", particles.split('\n')[0])[1]),
+                [(int(sid), (float(x), float(y), float(z)))
+                for _, sid, x, y, z in [particle.split()
+                for particle in particles.split('\n')[2:] if particle]])
+                for particles in open(
+                    "{}/{}.out".format(
+                        config["data_folder"],
+                        config["cell_file"].format(
+                            step=mech_step,
+                            substep=kin_step,
+                            cell_id=cell_id
+                            )
+                        )
+                    ).read().split("ParticlePositions")[1:]],
+            "cell": network[cell_id]["position"]}
+                for cell_id in network}
 
-def animate(i):
-    tstep = i // n_local_steps + 1 # Global timestep
-    T = i % n_local_steps # Local timestep
+        #kin_step+mech_step*kin_step
+        formated_data = np.array(sum([
+            [[
+                x + raw_data[cell_id]["cell"][0],
+                y + raw_data[cell_id]["cell"][1],
+                color_map[sid],
+                (1*(z/network[cell_id]["wsize"]) + bias),
+                ]
+                for sid, (x, y, z) in raw_data[cell_id]["particles"][0][1]]
+                for cell_id in raw_data], []))
 
-    if not T:
-        print(tstep)
-        for cell_id in network:
-            raw_data[cell_id] ={
-                "particles":[(float(re.split(" [TN]=", particles.split('\n')[0])[1]),
-                    [(int(sid), (float(x), float(y), float(z)))
-                    for _, sid, x, y, z in [particle.split()
-                    for particle in particles.split('\n')[2:] if particle]])
-                    for particles in open(config["data_folder"]+"/cell-{}-{}.out".format(tstep, cell_id),
-                        'r').read().split("ParticlePositions")[1:]],
-                "cell": network[cell_id]["position"]
-                }
 
-    formated_data = np.array(sum([
-        [[
-            x + raw_data[cell_id]["cell"][0],
-            y + raw_data[cell_id]["cell"][1],
-            color_map[sid],
-            (100*(z/network[cell_id]["wsize"]) + bias),
-            ]
-            for sid, (x, y, z) in raw_data[cell_id]["particles"][T][1]]
-            for cell_id in raw_data], []))
+        x, y, c, s = formated_data.T
+        plt.scatter(x.astype(float), y.astype(float), s=s.astype(float), c=c, alpha=0.3)
+        plt.xlim(xmin, xmax)
+        plt.ylim(ymin, ymax)
+        plt.savefig("{}/{}.png".format(
+            output_folder,
+            "network-{}".format(kin_step+mech_step*config["n_kin_substeps"])))
+        plt.close()
 
-    x, y, c, s = formated_data.T
-    plot.set_offsets(np.array(list(zip(x,y))))
-    plot.set_sizes(s.astype(float))
-    plot.set_color(c)
-    return plot
-
-ani = animation.FuncAnimation(fig, animate, range(config["n_steps"]*n_local_steps),
-        interval=10, init_func=init)
-ani.save("notchdelta.mp4")
